040de2ef1c87508a199e34484ded8957374754a2
mustneerar7 <mustneerahmad@outlook.com>
2023-02-09 16:28:49 +0500

    fragment_plant view created

--- app/src/main/java/com/example/treecycle/presentation/plant/PlantFragment.kt
+++ app/src/main/java/com/example/treecycle/presentation/plant/PlantFragment.kt
@@ -1,11 +1,15 @@
 package com.example.treecycle.presentation.plant
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.treecycle.R
 import com.example.treecycle.databinding.FragmentPlantBinding
+import com.example.treecycle.presentation.plantTree.PlantTreeActivity
+import com.google.android.material.card.MaterialCardView
 
 class PlantFragment : Fragment() {
 
@@ -27,6 +31,16 @@ class PlantFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        /* Accessing Plant A Tree card button via id.*/
+        val plantATreeButton : MaterialCardView = view.findViewById(R.id.plantTreeCardButton)
+
+        plantATreeButton.setOnClickListener {
+
+            /* Launching [PlantTreeActivity]. */
+            val intent = Intent(this.context, PlantTreeActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
---
+++ app/src/main/java/com/example/treecycle/presentation/plantTree/PlantTreeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.treecycle.presentation.plantTree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.treecycle.R
+
+class PlantTreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant_tree)
+    }
+}
